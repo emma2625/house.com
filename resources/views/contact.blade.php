b7a4d183218f9df2e863db8573dbdf5f0f882e44
emma2625 <emmanuelodobo10@gmail.com>
2024-11-30 17:04:19 +0100

    Updated Contact Page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,44 @@
 @extends('layouts.app')
+@section('content')
+    <section>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <div class="card mx-auto" style="max-width: 700px;">
+                        <div class="card-body">
+                            <form action="">
+                                <div class="mb-2">
+                                    <h1 class="text-center">Get in touch with us</h1>
+                                </div>
+        
+                                <div class="mb-2">
+                                    <label for="" class="form-label">Full Name</label>
+                                    <input type="text" class="form-control">
+                                </div>
+                                <div class="mb-2">
+                                    <label for="" class="form-label">Location</label>
+                                    <select name="" class="form-select">
+                                        <option value="">Lagos</option>
+                                        <option value="">Abuja</option>
+                                    </select>
+                                </div>
+        
+                                <div class="mb-2">
+                                    <label for="" class="form-label">Message</label>
+                                    <textarea rows="5" class="form-control"></textarea>
+                                </div>
+        
+                                <div class="mb-3">
+                                    <button class="btn btn-primary">Send Message</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <img src="{{ asset('images/img-2.2.jpg') }}" alt="" class="w-100">
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
